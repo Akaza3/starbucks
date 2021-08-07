4037939dd5c619713de7d1b03cb8ad8f347a7f3f
Akaza3 <doths@naver.com>
2021-08-08 07:37:03 +0900

    로그인 페이지 추가

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,3 @@
-const searchEl = document.querySelector('.search'); //자바스크립트 에서 document는 HTML 자체라고 봐도 무방하다.
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 // SCROLL AND BADGE
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -146,6 +128,3 @@ spyEl.forEach( function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); 
